b5bc8ffdc679c47112bfb4919654654411db2b85
Bidbogs-prog <haythamchhilif@gmail.com>
2025-01-24 00:36:08 +0100

    added forms to capture input values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+
+type Income = {
+  income: string;
+};
+
+type Expense = {
+  expense: string;
+};
 
 export default function Home() {
+  const [income, setIncome] = useState("");
+  const [expense, setExpense] = useState("");
+
+  const handleSubmit = function (e: React.FormEvent) {
+    e.preventDefault();
+  };
+
   return (
-    <div className="w-[100%] flex justify-center gap-5 relative h-screen">
+    <div className="w-[100%] grid grid-cols-2 divide-x-2 gap-5 relative h-screen divide-black">
       {" "}
-      <div className="Income w-[300px] flex flex-col gap-3 absolute left-1">
+      <div className="Income w-[300px] ml-[20px]">
         Income
-        <Input />
-        <Button> Add your Income</Button>
+        <form className="flex flex-col gap-3">
+          <Input
+            onChange={(e) => {
+              setIncome(e.target.value);
+            }}
+          />
+          <Button> Add your Income</Button>
+        </form>
       </div>
-      <div className="Expenses w-[300px] flex flex-col gap-3">
+      <div className="Expenses w-[300px] pl-[20px]">
         Expense
-        <Input />
-        <Button> Add your Expenses here</Button>
+        <form className="flex flex-col gap-3">
+          <Input
+            onChange={(e) => {
+              setExpense(e.target.value);
+            }}
+          />
+          <Button type="submit"> Add your Expenses here</Button>
+        </form>
       </div>
-      <div className="absolute bg-black border-black h-[100%] w-[0.1%] left-[750px]"></div>
     </div>
   );
 }
